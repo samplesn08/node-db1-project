aafdbe79722cbed4fcb07be9ecce06153024f8e7
samplesn08 <samplesn08@gmail.com>
2021-04-12 14:11:55 -0400

    model complete

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,5 +1,11 @@
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  const {title, contents} = req.body
+  if(title && contents){
+    next()
+  }else{
+    res.status(400).json({message:"title and contents required"})
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
@@ -8,4 +14,15 @@ exports.checkAccountNameUnique = (req, res, next) => {
 
 exports.checkAccountId = (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    const account = await Account.getById(req.params.id)
+    if(account){
+      req.account = account
+      next()
+    }else{
+      res.status(404).json({message:"Account not found"})
+    }
+  }catch(err){
+    next(err)
+  }
 }
